15b2c2e24dbd7903e756ed52f752370c848ca7c6
Mihail Iarov <yarovmk@gmail.com>
2019-10-24 13:40:52 +0100

    Added tests for divide method

--- src/main/java/ua/com/foxminded/longdivision/Calculator.java
+++ src/main/java/ua/com/foxminded/longdivision/Calculator.java
@@ -1,9 +1,13 @@
 package ua.com.foxminded.longdivision;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class Calculator {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+       int[] a = new int[] {2, 3, 5};
+       int[] b = new int[] {2, 3, 5};
+        assertArrayEquals(a,b);
 
     }
 
---
+++ src/test/java/ua/com/foxminded/longdivision/drawers/GraphicalDividerTest.java
@@ -0,0 +1,48 @@
+package ua.com.foxminded.longdivision.drawers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class GraphicalDividerTest {
+    private static DivisionMate divisionMate;
+    
+    @BeforeAll
+    public static void initialise() {
+        divisionMate = new DivisionMate();
+    }
+
+    @Test
+    public void divide_NegativeNumberAs1stParam_IllegalArgumentExceptionThrown() {
+        assertThrows(IllegalArgumentException.class, () -> divisionMate.divide(-4, 6));
+    }
+    
+    @Test
+    public void divide_NegativeNumberAs2ndParam_IllegalArgumentExceptionThrown() {
+        assertThrows(IllegalArgumentException.class, () -> divisionMate.divide(2, -8));
+    }
+    
+    @Test
+    public void divide_ZeroAs2ndParam_ArithmeticExceptionThrown() {
+        assertThrows(ArithmeticException.class, () -> divisionMate.divide(4, 0));
+    }
+    
+    
+    @ParameterizedTest(name = "{0} divided by {1} is {2} and {3} remainder")
+    @CsvSource({ 
+        "3, 2, 1, 1",
+        "0, 30, 0, 0",
+        "6, 4, 1, 2",
+        "43, 20, 2, 3"
+
+    })
+    void divide_SouldReverseLettersWhenPassedLettersOnly(int dividend, int divisor, int expectedQuotient, int expectedRemainder) {
+        int[] expectedResult = new int[] {expectedQuotient, expectedRemainder};
+        int[] actualResult = divisionMate.divide(dividend, divisor);
+        assertArrayEquals(expectedResult, actualResult);
+    }
+
+}
