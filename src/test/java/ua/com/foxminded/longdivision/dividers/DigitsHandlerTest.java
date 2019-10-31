2c4bdec6d84f5aecc36fb105e87a6b90da995fe7
Mihail Iarov <yarovmk@gmail.com>
2019-10-31 22:06:47 +0000

    Renamed digitBuilder to digitHandler

--- src/test/java/ua/com/foxminded/longdivision/dividers/DigitsHandlerTest.java
+++ src/test/java/ua/com/foxminded/longdivision/dividers/DigitsHandlerTest.java
@@ -9,17 +9,17 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class DigitsHandlerTest {
 
-    private static DigitsHandler digitBuilder;
+    private static DigitsHandler digitHandler;
 
     @BeforeAll
     public static void initialise() {
-        digitBuilder = new DigitsHandler();
+        digitHandler = new DigitsHandler();
     }
 
     @ParameterizedTest(name = "number {0} consists of {1} digits")
     @CsvSource({ "0, 1", "23, 2", "560, 3", "4647210, 7" })
     void testGetNumberOfDigits(int input, int expectedOutput) {
-        int actualOutput = digitBuilder.getNumberOfDigits(input);
+        int actualOutput = digitHandler.getNumberOfDigits(input);
         assertEquals(expectedOutput, actualOutput);
     }
 
@@ -27,7 +27,7 @@ class DigitsHandlerTest {
     void testGetDigits() {
         int number = 234678;
         int[] expected = { 2, 3, 4, 6, 7, 8 };
-        int[] actual = digitBuilder.getDigits(number);
+        int[] actual = digitHandler.getDigits(number);
         assertArrayEquals(expected, actual);
     }
 
@@ -37,7 +37,7 @@ class DigitsHandlerTest {
         int[] newDigits = { 5, 9, 3 };
         int[] digits = { 7, 9, 0, 3, 4, 2, 4 };
         int[] expected = { 7, 9, 0, 5, 9, 3, 4 };
-        int[] actual = digitBuilder.replaceDigits(digits, startIndex, newDigits);
+        int[] actual = digitHandler.replaceDigits(digits, startIndex, newDigits);
         assertArrayEquals(expected, actual);
 
     }
