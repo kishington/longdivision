2181c8c16280504721f6cf3668597f35e344cef3
Mihail Iarov <yarovmk@gmail.com>
2019-11-04 12:55:45 +0000

    Modified tests for visualise method

--- src/test/java/ua/com/foxminded/longdivision/dividers/DivisionVisualiserTest.java
+++ src/test/java/ua/com/foxminded/longdivision/dividers/DivisionVisualiserTest.java
@@ -1,8 +1,9 @@
 package ua.com.foxminded.longdivision.dividers;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
 
 class DivisionVisualiserTest {
     private static DivisionVisualiser divisionVisualiser;
@@ -11,20 +12,119 @@ class DivisionVisualiserTest {
     public static void initialise() {
         divisionVisualiser = new DivisionVisualiser();
     }
-
-    @ParameterizedTest
-    @CsvSource({ "78945, 4", "168345, 6", "0, 453", "25263263, 26302", "688438887, 1", "75499764, 75499764",
-            "100000, 100", "120, 6" })
-    void testDrawFirstThreeLines(int dividend, int divisor) {
-        divisionVisualiser.drawFirstThreeLines(dividend, divisor);
-        System.out.println();
+    
+    @Test
+    void testVisualise() {
+        int dividend = 78945;
+        int divisor = 4;
+        String expectedOutput = 
+                "_78945|4\n" +
+                " 4    |-----\n" +
+                " -    |19736\n" +
+                "_38\n" +
+                " 36\n" +
+                " --\n" +
+                " _29\n" +
+                "  28\n" +
+                "  --\n" +
+                "  _14\n" +
+                "   12\n" +
+                "   --\n" +
+                "   _25\n" +
+                "    24\n" +
+                "    --\n" +
+                "     1\n";
+        String actualOutput = divisionVisualiser.visualise(dividend, divisor);
+        assertEquals(expectedOutput,actualOutput);
     }
-
-    @ParameterizedTest
-    @CsvSource({ "78945, 4", "168345, 6", "0, 453", "25263263, 26302", "688438887, 1", "75499764, 75499764",
-            "100000, 100", "120, 6" })
-    void testVisualiseLongDivisionAlgorithm(int dividend, int divisor) {
-        divisionVisualiser.visualiseLongDivisionAlgorithm(dividend, divisor);
-        System.out.println();
+    
+    @Test
+    void testVisualise_dividendIsZero() {
+        int dividend = 0;
+        int divisor = 453;
+        String expectedOutput = 
+                "_0|453\n" +
+                " 0|---\n" +
+                " -|0\n" +
+                " 0\n";
+        String actualOutput = divisionVisualiser.visualise(dividend, divisor);
+        assertEquals(expectedOutput,actualOutput);
+    }
+    
+    @Test
+    void testVisualise_bigNumbers() {
+        int dividend = 25276229;
+        int divisor = 26302;
+        String expectedOutput = 
+                "_25276229|26302\n" + 
+                " 236718  |-----\n" + 
+                " ------  |961\n" + 
+                " _160442\n" + 
+                "  157812\n" + 
+                "  ------\n" + 
+                "   _26309\n" + 
+                "    26302\n" + 
+                "    -----\n" + 
+                "        7\n";
+        String actualOutput = divisionVisualiser.visualise(dividend, divisor);
+        assertEquals(expectedOutput,actualOutput);
+    }
+    
+    @Test
+    void testVisualise_divisorIsOne() {
+        int dividend = 68843;
+        int divisor = 1;
+        String expectedOutput = 
+                "_68843|1\n" + 
+                " 6    |-----\n" + 
+                " -    |68843\n" + 
+                " _8\n" + 
+                "  8\n" + 
+                "  -\n" + 
+                "  _8\n" + 
+                "   8\n" + 
+                "   -\n" + 
+                "   _4\n" + 
+                "    4\n" + 
+                "    -\n" + 
+                "    _3\n" + 
+                "     3\n" + 
+                "     -\n" + 
+                "     0\n";
+        String actualOutput = divisionVisualiser.visualise(dividend, divisor);
+        assertEquals(expectedOutput,actualOutput);
+    }
+    
+    @Test
+    void testVisualise_dividendEqualsDivisor() {
+        int dividend = 75499764;
+        int divisor = 75499764;
+        String expectedOutput = 
+                "_75499764|75499764\n" + 
+                " 75499764|--------\n" + 
+                " --------|1\n" + 
+                "        0\n";
+        String actualOutput = divisionVisualiser.visualise(dividend, divisor);
+        assertEquals(expectedOutput,actualOutput);
+    }
+    
+    @Test
+    void testVisualise_dividendAndDivisorMultiplesOfTen() {
+        int dividend = 10000;
+        int divisor = 100;
+        String expectedOutput = 
+                "_10000|100\n" + 
+                " 100  |---\n" + 
+                " ---  |100\n" + 
+                "   _0\n" + 
+                "    0\n" + 
+                "    -\n" + 
+                "    _0\n" + 
+                "     0\n" + 
+                "     -\n" + 
+                "     0\n";
+        String actualOutput = divisionVisualiser.visualise(dividend, divisor);
+        System.out.println(divisionVisualiser.visualise(dividend, divisor));
+        assertEquals(expectedOutput,actualOutput);
     }
 }
